58dcc5020013fa149dcd9549dfaf8dc45e55f0a1
Kosta45 <konstanlebed8@gmail.com>
2024-12-03 13:01:34 +0100

    Types were added in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import filterSlice from './slices/filterSlice';
-import basketSlice from './slices/basketSlice';
-import pizzaSlice from './slices/pizzasSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import filterSlice from "./slices/filterSlice";
+import basketSlice from "./slices/basketSlice";
+import pizzaSlice from "./slices/pizzasSlice";
 
 export const store = configureStore({
-    reducer: {
-        filter: filterSlice,
-        basket: basketSlice,
-        pizza: pizzaSlice
-    },
-})
+  reducer: {
+    filter: filterSlice,
+    basket: basketSlice,
+    pizza: pizzaSlice,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
