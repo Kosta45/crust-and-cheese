2ccba8761378be478ee7512747de3bbcbd6e58be
Kosta45 <konstanlebed8@gmail.com>
2024-12-26 20:17:22 +0100

    disablePageScroll, enablePageScroll, handleMouseScrollX are added

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,6 +4,7 @@ type CategoriesProps = {
   value: number;
   onChangeCategory: (index: number) => void;
 };
+
 const categories: string[] = [
   "Все",
   "Мясные",
@@ -16,9 +17,31 @@ const categories: string[] = [
 const Categories: FC<CategoriesProps> = memo(({ value, onChangeCategory }) => {
   //refs
   const categoriesRef = useRef<HTMLUListElement>(null);
+  const scrollableRef = useRef<HTMLDivElement>(null);
+
+  const disablePageScroll = () => {
+    document.body.style.transition = "overflow 0.3s easy";
+    document.body.style.overflow = "hidden";
+  };
+  const enablePageScroll = () => {
+    document.body.style.transition = "overflow 0.3s easy";
+    document.body.style.overflow = "";
+  };
+  const handleMouseScrollX = (event: React.WheelEvent<HTMLDivElement>) => {
+    const scrollContainer = scrollableRef.current;
+    if (scrollContainer) {
+      scrollContainer.scrollLeft += event.deltaY - 30;
+    }
+  };
 
   return (
-    <div className="categories">
+    <div
+      className="categories"
+      ref={scrollableRef}
+      onMouseEnter={disablePageScroll}
+      onWheel={handleMouseScrollX}
+      onMouseLeave={enablePageScroll}
+    >
       <ul ref={categoriesRef}>
         {categories.map((categoryName, index) => {
           return (
