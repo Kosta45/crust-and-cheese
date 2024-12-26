da0838752063e8312aa260b9dc7225a3b226711e
Kosta45 <konstanlebed8@gmail.com>
2024-12-26 20:24:16 +0100

    fixed BasketItem import ".../slices/basket/types"

--- src/utils/calcTotalPrice.ts
+++ src/utils/calcTotalPrice.ts
@@ -1,5 +1,4 @@
-import { BasketItem } from "../redux/slices/basketSlice";
-
+import { BasketItem } from "../redux/slices/basket/types";
 export const calcTotalPrice = (items: BasketItem[]) => {
   return items.reduce((sum, item) => {
     return item.price * item.count + sum;
